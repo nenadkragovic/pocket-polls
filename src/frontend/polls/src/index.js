acb242824047fb0677a6e18c2ba933f62838a4cf
Nenad Kragovic <nenad.kragovic@fazi.rs>
2022-11-30 23:04:17 +0100

    Fix import path

--- src/frontend/polls/src/index.js
+++ src/frontend/polls/src/index.js
@@ -7,7 +7,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import * as serviceWorkerRegistration from '../../serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
